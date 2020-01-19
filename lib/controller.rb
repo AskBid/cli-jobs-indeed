ee8701206593b6bc27819091f94cdd6c84023b16
Sergio Marelli <sergio.mrll@gmail.com>
2020-01-19 10:48:06 +0000

    CLI can now merge .searches and list .searches

--- lib/controller.rb
+++ lib/controller.rb
@@ -2,7 +2,6 @@ class JobsIndeedController
 
 	def initialize
 		String.disable_colorization = false
-		puts String.color_samples 
 
 		puts ""
 		puts ""
@@ -56,12 +55,30 @@ class JobsIndeedController
   end
 
   def merge_searches
-  	list_searches
+  	index = 1
+  	if Search.all.size > 1
+	  	list_searches
+	  	puts "Select which (index) search you want to extend:".colorize(:yellow)
+	  	puts "...".colorize(:light_blue)
+	  	index = gets.strip.to_i
+	  	if index < 1 && index > Search.all.size
+	  		merge_searches
+	  	end
+	  end
+	  index =- 1
+	  initial_jobs = Search.all[index].jobs.size
+	  Search.all[index].merge(input_search)
+
+	  puts ""
+	  print "The extended search added ".colorize(:yellow)
+	  puts "#{Search.all[index].jobs.size - initial_jobs} jobs".colorize(:light_red)
+	  puts ""
   end
 
   def main_menu
   	puts ""
-    puts "chose an action (enter number):".colorize(:light_yellow)
+    print "chose an action:".colorize(:light_yellow)
+    puts " (enter number)".colorize(:light_black)
     puts ""
     print "1: ".colorize(:light_yellow)
     puts "make another search".colorize(:yellow)
@@ -74,6 +91,10 @@ class JobsIndeedController
     print "5: ".colorize(:light_yellow)
     puts "get highest paid job".colorize(:yellow)
     print "6: ".colorize(:light_yellow)
+    puts "check company rating".colorize(:yellow)
+    print "7: ".colorize(:light_yellow)
+    puts "List searches".colorize(:yellow)
+    print "8: ".colorize(:light_yellow)
     puts "quit! (your job?)".colorize(:yellow)
     puts "...".colorize(:light_blue)
     action = gets.strip
@@ -84,8 +105,13 @@ class JobsIndeedController
     	when '2'
     		merge_searches
     		main_menu
-    	when '6'
+    	when '7'
+    		list_searches
+    		main_menu
+    	when '8'
+    		puts "!!!         !!!".colorize(:light_blue)
     		puts 'We wish you the best of luck! :)'.colorize(:light_magenta)
+    		puts "!!! the end !!!".colorize(:light_blue)
     	else
     		puts 'you insert the wrong action/input'.colorize(:light_magenta)
     		puts 'please choose between one of the listed numbers:'.colorize(:light_magenta)
@@ -95,15 +121,17 @@ class JobsIndeedController
 
   def list_searches
   	puts ""
-  	     "------------------------------------------------------------".colorize(:light_blue)
-  	puts "::::::::::::::::::: existing searches ::::::::::::::::::::::".colorize(:light_blue)
+  	puts "----------------------------------------------------------".colorize(:light_blue)
+  	puts "::::::::::::::::::: existing searches ::::::::::::::::::::".colorize(:light_blue)
   	Search.all.each_with_index {|search, i| 
   		puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".colorize(:light_blue)
   		puts "#{i + 1}.".colorize(:light_yellow)
-  		print "title: ".colorize(:light_black)
+  		print "  title: ".colorize(:light_black)
   		puts "#{search.position}".colorize(:red)
-  		print "in   : ".colorize(:light_black)
+  		print "  in   : ".colorize(:light_black)
   		puts "#{search.city}".colorize(:red)
+  		print "  jobs : ".colorize(:light_black)
+  		puts "#{search.jobs.size}".colorize(:red)
   	}
   	puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~".colorize(:light_blue)
   end
